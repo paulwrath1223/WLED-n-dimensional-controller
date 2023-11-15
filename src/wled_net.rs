af895d4ef59e1f981604df3188b20a200bac45fb
Paul Fornage <paul@fornage.net>
2023-11-15 13:35:02 -0800

    not working

--- src/wled_net.rs
+++ src/wled_net.rs
@@ -1,27 +1,42 @@
 use wled_json_api_library;
 use ddp_rs;
 use wled_json_api_library::structures::cfg::cfg_hw::cfg_hw_led::Led as CfgLed;
+use wled_json_api_library::structures::cfg::cfg_id::Id as CfgId;
+use wled_json_api_library::structures::cfg::cfg_hw::cfg_hw_led::LightCapability;
 use wled_json_api_library::errors::WledJsonApiError;
+use ddp_rs::connection::DDPConnection;
 
-struct PhysicalWled {
+/// A single WLED server.
+pub struct PhysicalWled<'a> {
+    /// the connection to the WLED JSON API. Library by me
     json_con: wled_json_api_library::wled::Wled,
-    ddp_con: ddp_rs::connection::DDPConnection,
-    segments: Vec<SegmentBounds>
+    /// the DDP connection. Library by @coral and some minor contributions by myself
+    ddp_con: DDPConnection,
+    /// information about pre-defined segments
+    segments: Vec<SegmentInfo<'a>>,
+    /// the name of the server (found in the WLED UI settings)
+    friendly_name: String,
+    /// the url to reach the WLED on in case the IP is not static. always append ".local"
+    local_url: String,
+    /// Local network IP, ipv4 only currently. (same as WLED)
+    local_ip: [u8; 4],
 }
 
-pub struct SegmentBounds{
+pub struct SegmentInfo<'a> {
     pub start: u16,
-    pub length: u16
+    pub length: u16,
+    pub name: String,
+    pub capabilities: LightCapability,
+    pub ddp_ref: &'a DDPConnection
 }
 
-pub fn get_segment_bounds(cfg_led: CfgLed) -> Result<Vec<SegmentBounds>, WledJsonApiError> {
+pub fn get_segment_bounds<'a>(cfg) -> Result<Vec<SegmentInfo<'a>>, WledJsonApiError> {
     let server_info = cfg_led.ins.ok_or(WledJsonApiError::MissingKey)?;
-    let mut bound_vec: Vec<SegmentBounds> = Vec::with_capacity(server_info.len());
+    let mut bound_vec: Vec<SegmentInfo> = Vec::with_capacity(server_info.len());
     for bus in server_info{
-        bound_vec.push(SegmentBounds
+        bound_vec.push(SegmentInfo
         {
-            start: bus.start.ok_or(WledJsonApiError::MissingKey)?,
-            length: bus.len.ok_or(WledJsonApiError::MissingKey)?
+
         })
     }
     Ok(bound_vec)
